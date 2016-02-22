9ccbc430fa69e77414b739ad9ee8a85ae7e1a3c8
Dave Hall <dave@etianen.com>
2016-02-22 11:31:25 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,233 @@
+# @etianen/dict
+
+Helpers for treating dicts as maps.
+
+
+## Installing
+
+``` bash
+npm install '@etianen/dict'
+```
+
+**Typescript:** To take advantage of typings, be sure to set `moduleResolution` to `"node"` in your `tsconfig.json`.
+
+
+## Overview
+
+Treating dicts as maps in Javascript is needlessly difficult.
+
+@etianen/dict provides a minimal set of helpers of interacting with dicts.
+
+
+``` ts
+import * as dict from "@etianen/dict";
+
+dict.has({foo: "bar"}, "foo");  // => true
+dict.mapValues({foo: "bar"}, v => v.toUpperCase());  // => {foo: "BAR"}
+```
+
+
+## API
+
+In all the functions below:
+
+* Only own, enumerable properties are considered.
+* The source arguments are never mutated.
+
+
+### Dict
+
+An object containing values of type V.
+
+``` ts
+type Dict<V> = {[key: string]: V};
+```
+
+
+### count()
+
+Returns the number of entries in `dict`.
+
+**Note:** This is an O(n) operation. Use `isEmpty()` if you just want to check if the dict is empty.
+
+``` ts
+function count(dict: Dict<Object>): number;
+```
+
+
+### entries()
+
+Returns an `Array` of `[key, value]` entries in `dict`.
+
+``` ts
+function entries<V>(dict: Dict<V>): Array<[string, V]>;
+```
+
+
+### every()
+
+Returns `true` if all entries in `dict` satisfy `predicate`.
+
+``` ts
+function every<V>(dict: Dict<V>, predicate: (value?: V, key?: string, dict?: Dict<V>) => boolean, context?: Object): boolean;
+```
+
+
+### filter()
+
+Returns a new dict containing entries from `dict` that satisfy `predicate`.
+
+``` ts
+function filter<V>(dict: Dict<V>, predicate: predicate: (value?: V, key?: string, dict?: Dict<V>) => boolean, context?: Object): Dict<V>
+```
+
+
+### forEach()
+
+Runs `sideEffect` for each entry in `dict`.
+
+``` ts
+function forEach<V>(dict: Dict<V>, sideEffect: (value?: V, key?: string, dict?: Dict<V>) => void, context?: Object): void;
+```
+
+
+### from()
+
+Creates a new dict from an array of `[key, value]` entries.
+
+``` ts
+function from<V>(entries: Array<[string, V]>): Dict<V>;
+```
+
+
+### get()
+
+Returns the value at `key` in `dict`, or `defaultValue` if the key is not set.
+
+``` ts
+function get<V>(dict: Dict<V>, key: string, defaultValue?: V): V;
+```
+
+
+### has()
+
+Returns `true` if `key` is in `dict`.
+
+``` ts
+function has(dict: Dict<Object>, key: string): boolean;
+```
+
+
+### isEmpty()
+
+Returns `true` if `dict` contains no entries.
+
+``` ts
+function isEmpty(dict: Dict<Object>): boolean;
+```
+
+
+### keys()
+
+Returns an `Array` of all keys in `dict`.
+
+``` ts
+function keys(dict: Dict<Object>): Array<string>;
+```
+
+
+### map()
+
+Returns a new `Array` with all entries passed through `mapper`.
+
+``` ts
+function map<V, R>(dict: Dict<V>, mapper: (value?: V, key: string, dict?: Dict<V>) => R, context?: Object): Array<R>
+```
+
+
+### mapEntries()
+
+Returns a new dict with all entries passed through `mapper`.
+
+``` ts
+function mapEntries<V, R>(dict: Dict<V>, mapper: (value?: V, key: string, dict?: Dict<V>) => [string, R], context?: Object): Dict<R>
+```
+
+
+### mapKeys()
+
+Returns a new dict with all keys passed through `mapper`.
+
+``` ts
+function mapKeys<V>(dict: Dict<V>, mapper: (key?: string, value? V, dict?: Dict<V>) => string, context?: Object): Dict<V>;
+```
+
+
+### mapValues()
+
+Returns a new dict with all values passed through `mapper`.
+
+``` ts
+map<V, R>(dict: Dict<V>, mapper: (value?: V, key?: string, dict?: Dict<V>) => R, context?: Object): Dict<R>;
+```
+
+
+### reduce()
+
+Reduces `dict` to a single value using `reducer`.
+
+``` ts
+function reduce<V>(dict: Dict<V>, reducer: (reduction?: V, value?: V, key?: string, dict?: Dict<V>) => V): V;
+function reduce<V, R>(dict: Dict<V>, reducer: (reduction?: R, value?: V, key?: string, dict?: Dict<V>) => R, initialReduction: R, context?: V;
+```
+
+
+### set()
+
+Returns a new dict with `key` set to `value`.
+
+``` ts
+function set<V>(dict: Dict<V>, key: string, value: V): Dict<V>;
+```
+
+
+### some()
+
+Returns `true` if one or more entries in `dict` satisfy `predicate`.
+
+``` ts
+function some<V>(dict: Dict<V>, predicate: (value?: V, key?: string, dict?: Dict<V>) => boolean, context?: Object): boolean;
+```
+
+
+### values()
+
+Returns an `Array` of all values in `dict`.
+
+``` ts
+function values<V>(dict: Dict<V>): Array<V>;
+```
+
+
+## Build status
+
+This project is built on every push using the Travis-CI service.
+
+[![Build Status](https://travis-ci.org/etianen/js-dict.svg?branch=master)](https://travis-ci.org/etianen/js-dict)
+
+
+## Support and announcements
+
+Downloads and bug tracking can be found at the [main project website](http://github.com/etianen/js-dict).
+
+
+## More information
+
+This project was developed by Dave Hall. You can get the code
+from the [project site](http://github.com/etianen/js-dict).
+
+Dave Hall is a freelance web developer, based in Cambridge, UK. You can usually
+find him on the Internet:
+
+- [Website](http://www.etianen.com/)
+- [Google Profile](http://www.google.com/profiles/david.etianen)
---
+++ test/index-test.ts
@@ -0,0 +1,267 @@
+import {expect} from "chai";
+import {Dict, count, entries, every, filter, forEach, from, get, has, isEmpty, keys, map, mapValues, mapEntries, mapKeys, reduce, set, some, values} from "../lib/index";
+
+
+describe("dict", () => {
+
+    const foo: Dict<number> = {foo: 1};
+
+    describe("count", () => {
+
+        it("counts own enumerable properties", () => {
+            expect(count(foo)).to.equal(1);
+        });
+
+    });
+
+    describe("entries", () => {
+
+        it("returns an array of entries", () => {
+            expect(entries(foo)).to.eql([["foo", 1]]);
+        });
+
+    });
+
+    describe("every", () => {
+
+        it("checks if every value passes a predicate", () => {
+            expect(every(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return value === 1;
+                },
+                "bar"
+            )).to.be.true;
+            expect(every({foo: 1, bar: 2}, value => value >= 1)).to.be.true;
+            expect(every({foo: 1, bar: 2}, value => value < 2)).to.be.false;
+        });
+
+    });
+
+    describe("filter", () => {
+
+        it("filters the dictect with a predicate", () => {
+            expect(filter(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return value === 1;
+                },
+                "bar"
+            )).to.eql({foo: 1});
+            expect(filter(foo, value => value === 2)).to.eql({});
+        });
+
+    });
+
+    describe("forEach", () => {
+
+        it("runs side effect for each entry", () => {
+            forEach(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                },
+                "bar"
+            );
+        });
+
+    });
+
+    describe("from", () => {
+
+        it("creates an dictect from an array of entries", () => {
+            expect(from([["foo", 1]])).to.eql(foo);
+        });
+
+    });
+
+    describe("get", () => {
+
+        it("gets own enumerable properties", () => {
+            expect(get(foo, "foo")).to.equal(1);
+        });
+
+        it("allows a default value to be given", () => {
+            expect(get(foo, "bar", 2)).to.equal(2);
+        });
+
+    });
+
+    describe("has", () => {
+
+        it("checks own enumberable properties", () => {
+            expect(has(foo, "foo")).to.be.true;
+        });
+
+        it("ignores inherited properties", () => {
+            expect(has(foo, "propertyIsEnumerable")).to.be.false;
+        });
+
+    });
+
+    describe("isEmpty", () => {
+
+        it("checks if an dictect is empty", () => {
+            expect(isEmpty(foo)).to.be.false;
+            expect(isEmpty({})).to.be.true;
+        });
+
+    });
+
+    describe("keys", () => {
+
+        it("returns an array of own enumerable properties", () => {
+            expect(keys(foo)).to.eql(["foo"]);
+        });
+
+    });
+
+    describe("map", () => {
+
+        it("returns an array with entries mapped", () => {
+            const result: Array<string> = map(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return `${key}${value}`;
+                },
+                "bar"
+            );
+            expect(result).to.eql(["foo1"]);
+        });
+
+    });
+
+    describe("mapValues", () => {
+
+        it("returns a new dictect with values mapped", () => {
+            const result: Dict<string> = mapValues(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return `${key}${value}`;
+                },
+                "bar"
+            );
+            expect(result).to.eql({foo: "foo1"});
+        });
+
+    });
+
+    describe("mapEntries", () => {
+
+        it("returns a new dictect with entries mapped", () => {
+            const result: Dict<string> = mapEntries(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return [`${key}bar`, `${key}${value}`];
+                },
+                "bar"
+            );
+            expect(result).to.eql({foobar: "foo1"});
+        });
+
+    });
+
+    describe("mapKeys", () => {
+
+        it("returns a new dictect with entries mapped", () => {
+            const result: Dict<number> = mapKeys(
+                foo,
+                function(key: string, value: number, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return `${key}bar`;
+                },
+                "bar"
+            );
+            expect(result).to.eql({foobar: 1});
+        });
+
+    });
+
+    describe("reduce", () => {
+
+        it("reduces an dictect to a value", () => {
+            const result: string = reduce(
+                foo,
+                function(reduction: string, value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return `${reduction}${key}`;
+                },
+                "reduction",
+                "bar"
+            );
+            expect(result).to.eql("reductionfoo");
+        });
+
+        it("allows a default initial reduction", () => {
+            expect(reduce({foo: 1,  bar: 2}, (total, value) => total + value)).to.equal(3);
+        });
+
+    });
+
+    describe("set", () => {
+
+        it("sets the value in a new dictect", () => {
+            expect(set(foo, "foo", 99)).to.eql({foo: 99});
+        });
+
+    });
+
+    describe("some", () => {
+
+        it("checks if at least one value passes a predicate", () => {
+            expect(some(
+                foo,
+                function(value: number, key: string, dict: Dict<number>) {
+                    expect(this).to.equal("bar");
+                    expect(value).to.equal(1);
+                    expect(key).to.equal("foo");
+                    expect(dict).to.equal(dict);
+                    return value === 1;
+                },
+                "bar"
+            )).to.be.true;
+            expect(some({foo: 1, bar: 2}, value => value >= 1)).to.be.true;
+            expect(some({foo: 1, bar: 2}, value => value < 2)).to.be.true;
+            expect(some({foo: 1, bar: 2}, value => value < 1)).to.be.false;
+        });
+
+    });
+
+    describe("values", () => {
+
+        it("returns an array of values", () => {
+            expect(values(foo)).to.eql([1]);
+        });
+
+    });
+
+});
